415e6e733aa8add628872b46985fbde6d32e7a92
dplocki <dawid.plocki@gmail.com>
2024-12-16 19:24:23 +0100

    feat: move originx and originy into space between component

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ document.addEventListener('alpine:init', () => {
     const canvas = document.getElementById('graphCanvas');
     const ctx = canvas.getContext('2d');
 
+    let originX = null;
+    let originY = null;
+
     Alpine.data('app', () => ({
         width: canvas.width,
         height: canvas.height,
         scaleX: 40,
         scaleY: 100,
-        originX: null,
-        originY: null,
+
         k: 1,
         formula: 'k * Math.sin(x)',
 
         init() {
-            this.originX = this.width / 2;
-            this.originY = this.height / 2;
+            originX = this.width / 2;
+            originY = this.height / 2;
             this.redrawGraph();
         },
 
@@ -22,11 +24,11 @@ document.addEventListener('alpine:init', () => {
             ctx.beginPath();
             ctx.strokeStyle = 'black';
 
-            ctx.moveTo(0, this.originY);
-            ctx.lineTo(this.width, this.originY);
+            ctx.moveTo(0, originY);
+            ctx.lineTo(this.width, originY);
 
-            ctx.moveTo(this.originX, 0);
-            ctx.lineTo(this.originX, this.height);
+            ctx.moveTo(originX, 0);
+            ctx.lineTo(originX, this.height);
 
             ctx.stroke();
 
@@ -34,22 +36,22 @@ document.addEventListener('alpine:init', () => {
             ctx.fillStyle = 'black';
             ctx.textAlign = 'center';
 
-            for (let x = -this.originX; x < this.width - this.originX; x += this.scaleX) {
-                const canvasX = this.originX + x;
-                ctx.moveTo(canvasX, this.originY - 5);
-                ctx.lineTo(canvasX, this.originY + 5);
+            for (let x = -originX; x < this.width - originX; x += this.scaleX) {
+                const canvasX = originX + x;
+                ctx.moveTo(canvasX, originY - 5);
+                ctx.lineTo(canvasX, originY + 5);
                 ctx.stroke();
-                ctx.fillText((x / this.scaleX).toFixed(1), canvasX, this.originY + 15);
+                ctx.fillText((x / this.scaleX).toFixed(1), canvasX, originY + 15);
             }
 
             ctx.textAlign = 'right';
-            for (let y = -this.originY; y < this.height - this.originY; y += this.scaleY) {
-                const canvasY = this.originY - y;
-                ctx.moveTo(this.originX - 5, canvasY);
-                ctx.lineTo(this.originX + 5, canvasY);
+            for (let y = -originY; y < this.height - originY; y += this.scaleY) {
+                const canvasY = originY - y;
+                ctx.moveTo(originX - 5, canvasY);
+                ctx.lineTo(originX + 5, canvasY);
                 ctx.stroke();
                 if (y !== 0) {
-                    ctx.fillText((y / this.scaleY).toFixed(1), this.originX - 10, canvasY + 3);
+                    ctx.fillText((y / this.scaleY).toFixed(1), originX - 10, canvasY + 3);
                 }
             }
         },
@@ -59,12 +61,12 @@ document.addEventListener('alpine:init', () => {
             ctx.strokeStyle = color;
 
             let first = true;
-            for (let x = -this.originX; x < this.width - this.originX; x += 0.1) {
+            for (let x = -originX; x < this.width - originX; x += 0.1) {
                 const mathX = x / this.scaleX;
                 const mathY = func(mathX);
 
-                const canvasX = this.originX + x;
-                const canvasY = this.originY - mathY * this.scaleY;
+                const canvasX = originX + x;
+                const canvasY = originY - mathY * this.scaleY;
 
                 if (first) {
                     ctx.moveTo(canvasX, canvasY);
